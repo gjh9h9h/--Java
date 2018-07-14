928a18b6a610768f714c19f60bdb71031fd80d4c
gjh9h9h <gjh9h9h@163.com>
2018-07-14 22:48:52 +0800

    project completed

---
+++ src/main/java/com/mmall/service/IFileService.java
@@ -0,0 +1,11 @@
+package com.mmall.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by XiaoHuihui on 2018/7/10.
+ */
+public interface IFileService {
+
+    String upload(MultipartFile file, String path);
+}
